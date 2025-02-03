2e59f36680e47c6dbacf2569062b648b69af8a9c
smallrussian <julian.shniter@gmail.com>
2025-02-03 13:15:03 -0600

    initialized capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.iris.ap',
+  appName: 'image-recognition-integration-system',
+  webDir: 'public'
+};
+
+export default config;
