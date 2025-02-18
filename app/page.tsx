94a82b94166993594506104d9d779eed0e2fb928
Megh Patel <megh@Meghs-MacBook-Air.local>
2025-02-18 11:14:47 -0600

    added navigation bar as a component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import CameraButton from "@/components/cameraButton";
+import NavigationBar from "@/components/navigationBar"
 
 export default function Home() {
   return (
@@ -22,20 +23,7 @@ export default function Home() {
           <CameraButton />
         </div>
       </div>
-      <nav className="absolute bottom-0 w-full bg-white border-t flex justify-around py-2 shadow-md">
-        <button className="flex flex-col items-center text-gray-700">
-          <div className="w-6 h-6 bg-gray-500 rounded-full"></div>
-          <span className="text-xs">Home</span>
-        </button>
-        <button className="flex flex-col items-center text-gray-700">
-          <div className="w-6 h-6 bg-gray-500 rounded-full"></div>
-          <span className="text-xs">Search</span>
-        </button>
-        <button className="flex flex-col items-center text-gray-700">
-          <div className="w-6 h-6 bg-gray-500 rounded-full"></div>
-          <span className="text-xs">Profile</span>
-        </button>
-      </nav>
+      <NavigationBar/>
     </div>
   );
 }
