1915b813c638ebab8306ab12c478c81c1ec74784
rmosad <ramiz.mosad@slu.edu>
2025-03-01 21:29:27 -0600

    debugging CI/CD issues

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 // jest.config.js
-module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleNameMapper: {
-      '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    },
-  };
-  
+const nextJest = require('next/jest');
+
+// Provide the path to Next.js app to load next.config.js and .env files in test environment
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  // Use jest-environment-jsdom for browser-like testing
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+
+// createJestConfig is exported to ensure that Next.js' babel configuration is applied
+module.exports = createJestConfig(customJestConfig);
