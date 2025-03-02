658a319324b7f7805931b62c500a9a73ccb6777b
rmosad <ramiz.mosad@slu.edu>
2025-03-01 20:58:52 -0600

    checking if CI/CD workflow is working

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+      '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    },
+  };
+  
