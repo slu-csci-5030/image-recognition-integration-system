a165190d247c0a20dd1791e4bd1ef0d41a8879bd
smallrussian <julian.shniter@slu.edu>
2025-03-24 13:08:39 -0500

    cleaned up link code

--- app/components/navigationBar.tsx
+++ app/components/navigationBar.tsx
@@ -1,30 +1,37 @@
 import Link from "next/link";
 
 const BottomNav = () => {
-    return (
-      <nav className="absolute bottom-0 w-full bg-white border-t flex justify-around py-2 shadow-md">
-  
-        <Link href="/" className="flex flex-col items-center text-gray-700">
+  return (
+    <nav className="absolute bottom-0 w-full bg-white border-t flex justify-around py-2 shadow-md">
+      <Link href="/">
+        <div className="flex flex-col items-center text-gray-700 cursor-pointer">
           <div className="w-6 h-6 bg-gray-500 rounded-full"></div>
           <span className="text-xs">Home</span>
-        </Link>
+        </div>
+      </Link>
 
-        <Link href="/profile" className="flex flex-col items-center text-gray-700">
+      <Link href="/profile">
+        <div className="flex flex-col items-center text-gray-700 cursor-pointer">
           <div className="w-6 h-6 bg-gray-500 rounded-full"></div>
           <span className="text-xs">Profile</span>
-        </Link>
+        </div>
+      </Link>
 
-        <Link href="/about" className="flex flex-col items-center text-gray-700">
+      <Link href="/about">
+        <div className="flex flex-col items-center text-gray-700 cursor-pointer">
           <div className="w-6 h-6 bg-gray-500 rounded-full"></div>
           <span className="text-xs">About</span>
-        </Link>
+        </div>
+      </Link>
 
-        <Link href="/previousImages" className="flex flex-col items-center text-gray-700">
+      <Link href="/previousImages">
+        <div className="flex flex-col items-center text-gray-700 cursor-pointer">
           <div className="w-6 h-6 bg-gray-500 rounded-full"></div>
           <span className="text-xs">Previous Images</span>
-        </Link>
-      </nav>
-    );
+        </div>
+      </Link>
+    </nav>
+  );
 };
 
 export default BottomNav;
