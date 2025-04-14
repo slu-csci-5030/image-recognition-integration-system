c927fbe1afaedec0e19d66849a5616d579f69cf3
smallrussian <julian.shniter@slu.edu>
2025-04-14 15:45:32 -0500

    feat: made the navbar display on every page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
+"use client"
+
 import CameraButton from "@/app/components/cameraButton";
 import NavigationBar from "@/app/components/navigationBar";
+import { AppConfig } from "@/types/config";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+
+  const [config, setConfig] = useState<AppConfig | null>(null);
+  useEffect(() => {
+    fetch(`/api/config`)
+      .then((response) => response.json())
+      .then((data) => {
+        setConfig(data);
+      }
+      )
+      .catch((error) => {
+        console.error("Error loading config:", error);
+      }
+      );
+  }, []);
+
+
   return (
     <div className="flex items-center justify-center h-screen relative">
-      <div className="flex flex-col items-center">
-        <h1 className="text-3xl font-semibold text-gray-800 mb-8 text-center">
+      <div className={`flex flex-col items-center`}>
+        <h1 className={`text-3xl font-semibold ${config?.textColor} mb-8 text-center`}>
           Click below to upload image
         </h1>
         <div className="flex space-x-4">
