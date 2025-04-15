b305c8fa975bc4253b6de03c323fac4b243112dc
smallrussian <julian.shniter@slu.edu>
2025-04-15 14:46:10 -0500

    added back the capcaitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.iris.app',
+  appName: 'image-recognition-integration-system',
+  webDir: 'out',
+};
+
+export default config;
